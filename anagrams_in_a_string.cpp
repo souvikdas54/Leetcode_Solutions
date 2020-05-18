29766e12c8dbb05026532c425ee1881694d92a01
souvikdas54 <85souvikdas@gmail.com>
2020-05-18 11:12:48 +0530

    Adding second problem

---
+++ anagrams_in_a_string.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string.h>
+#include<vector>
+
+using namespace std;
+
+vector<int> anagrams(string s,string p){
+    vector<int> sv(26,0),pv(26,0),res;
+    if(p.size()>s.size())     //base case
+        return res;
+    for(int i =0;i<p.size();i++){
+        ++sv[s[i]-'a'];
+        ++pv[p[i]-'a'];
+    }
+    if(pv==sv)
+        res.push_back(0);
+    for(int i=p.size();i<s.size();i++){
+        ++sv[s[i]-'a'];
+        --sv[s[i-p.size()]-'a'];
+        if(sv==pv)            //This comparison happens in O(1) time
+            res.push_back(i-p.size()+1);
+    }
+    return res;
+}
+
+int main()
+{
+    string s = "abcaskwqwmdoqdmqowdjqowdjqwjdoqwdjoas";
+    string p = "owd";
+    vector<int> result = anagrams(s,p);
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
